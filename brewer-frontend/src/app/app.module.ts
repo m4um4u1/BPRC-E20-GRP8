904828f3ab5a966fe74c665220caa9ac96bd9ab0
Maurice <m4um4u1@gmail.com>
2020-10-21 16:10:49 +0200

    added grid to the frontpage with placeholders

--- brewer-frontend/src/app/app.module.ts
+++ brewer-frontend/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FrontpageComponent } from './pages/frontpage/frontpage.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { FrontpageComponent } from './pages/frontpage/frontpage.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
