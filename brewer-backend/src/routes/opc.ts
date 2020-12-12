54b4d73079a147249b0f6034cfc293a0e126a481
Maurice <m4um4u1@gmail.com>
2020-12-12 16:11:40 +0100

    added subscription to get one of the opcua values

--- brewer-backend/src/routes/opc.ts
+++ brewer-backend/src/routes/opc.ts
@@ -1,18 +1,12 @@
 import express from 'express'
-import OpcuaClient from "../OpcuaClient";
-import { AttributeIds } from 'node-opcua'
-const router = express.Router();
+import { Subscriptions } from "../Subscriptions";
 
-const client = new OpcuaClient()
+const router = express.Router();
+const sub = new Subscriptions()
 
 router.get("/batch/value", async (req, res)=> {
-    const session = await client._init();
-    const dataValue = await session.read({
-        nodeId: "ns=6;s=::Program:Cube.Command.Parameter[0].Value",
-        attributeId: AttributeIds.Value
-    });
-    console.log(" value = ", dataValue.value);
-})
+    console.log(sub.batchValue());
+});
 
 router.get("/batch/")
 
