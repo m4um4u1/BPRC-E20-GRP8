f255b6c9aaffd0f4fe4710f8e032e00be14a4c16
Maurice <m4um4u1@gmail.com>
2020-12-13 18:35:09 +0100

    kafka

--- brewer-backend/src/index.ts
+++ brewer-backend/src/index.ts
@@ -1,21 +1,3 @@
-import express from 'express';
-import cors from 'cors'
-import routes from './routes'
+import {Subscriptions} from "./Subscriptions";
 
-const app = express();
-const PORT = 3001
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cors());
-
-app.use('/', routes);
-
-app.listen(PORT, ()=> console.log(`Running on Port ${ PORT }`));
-
-
-
-
-
-
+const sub = new Subscriptions();
